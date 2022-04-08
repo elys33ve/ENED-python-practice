740df693912c376b4299f8fd2016c850b20042de
elys33ve <elys33ve@gmail.com>
2022-04-08 17:17:08 -0400

    subtask1_p1.py

--- ened2-project5/subtask1_p1.py
+++ ened2-project5/subtask1_p1.py
@@ -1,41 +1,38 @@
 #!/usr/bin/env python3
 
-from csv import list_dialects
-import os
-import sys
+# Move forward a given distance and move backwards to original position 
+# for a given number of laps.
+
 import math as m
 from time import sleep
-
-
-import ev3dev2 as e
-import ev3dev2.motor as em
 
 from ev3dev2.motor import OUTPUT_B, OUTPUT_D                      #motor ports
 
 from ev3dev2.motor import LargeMotor, MoveSteering
 ##################################################
-# Forward, back in straigbt line
 
-
-####
-
-r = OUTPUT_D
-l = OUTPUT_B
+r = OUTPUT_D    #right motor port
+l = OUTPUT_B    #left motor port
 r_motor = LargeMotor(r)
 l_motor = LargeMotor(l)
 
+steer = MoveSteering(l, r)
+
+# constants
+r = 2.75                #radius (cm)
 angle = 0
 speed = 50
 
-steer = MoveSteering(l, r)
+# dependent variables
+dist = 120                  #distance in cm
+laps = 3                    #number of laps
 
-dist = 120
-laps = 3
 
-rotations = dist / (2.75 * 2 * m.pi)
+#####
+rotations = dist / (2 * m.pi * r)   #distance / wheel circumference = num of rotations
 
 for i in range(laps):
-    steer.on_for_rotations(0, speed, rotations)                 # 0=straight, -=left, +=right
+    steer.on_for_rotations(0, speed, rotations)         # 0=straight, -=left, +=right
     sleep(0.5)
     steer.on_for_rotations(0, speed*-1, rotations)
     sleep(0.5)

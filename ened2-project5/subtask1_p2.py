8476075461c12dc28a899a50271019acd6565c48
elys33ve <elys33ve@gmail.com>
2022-04-08 17:18:49 -0400

    subtask1_p2.py

--- ened2-project5/subtask1_p2.py
+++ ened2-project5/subtask1_p2.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
-import math as m
+# Move forward a given distance, turn 180 degrees and return to original
+# position a given number of times.
+
 from time import sleep
+import math as m
 
 from ev3dev2.motor import OUTPUT_B, OUTPUT_D                      #motor ports
 
@@ -9,34 +12,46 @@ from ev3dev2.sensor.lego import GyroSensor
 from ev3dev2.motor import LargeMotor, MoveSteering
 ##################################################
 # forward in straght line, turn 180 degrees, forward for same distance
+##################################################
 
-MODE_GYRO_ANG = 'GYRO-ANG'
+MODE_GYRO_ANG = 'GYRO-ANG'          #set gryo mode to angle
 
 gyro = GyroSensor()
-gyro.reset()
+gyro.reset()            #reset gyro position
 
 
-r = OUTPUT_D
-l = OUTPUT_B
-r_motor = LargeMotor(r)
-l_motor = LargeMotor(l)
+r = OUTPUT_D    #right motor port
+l = OUTPUT_B    #left motor port
+r_motor = LargeMotor(r)                     #right motor
+l_motor = LargeMotor(l)                     #left motor
 
-steer = MoveSteering(l, r)
-lm = LargeMotor()
+lm = LargeMotor()                           #both motors
+steer = MoveSteering(l, r)                  #both motors steer
 
+# constants
+r = 2.75                #radius (cm)
 speed = 50
-sp_l = 25
-sp_r = -25
+sp_l = 25           #left +
+sp_r = -25          #right -
 
-dist = 90
-laps = 4 * 2
+# dependent variables
+dist = 90                   #distance in cm
+laps = 4                    #number of laps
 
-rotations = dist / (2.75 * 2 * m.pi)
+
+#####
+laps = laps * 2                         #double amt laps
+rotations = dist / (2.75 * 2 * m.pi)    #dist / circumference
+
 
 for i in range(laps):
-    steer.on_for_rotations(0, speed, rotations)                 # 0=straight, -=left, +=right
+    steer.on_for_rotations(0, speed, rotations)         #forward for rotations
     sleep(0.5)
+    gyro.reset()                                            #reset gyro
+    while gyro.angle > -178:                            #until approx. 180 deg
+        steer.on_for_rotations(-50, speed, rotations)       #turn to left
+    steer.on_for_rotations(0, speed, rotations)
     gyro.reset()
     while gyro.angle > -178:
-        lm.on(sp_l,sp_r)
+        steer.on_for_rotations(-50, speed, rotations)
 
